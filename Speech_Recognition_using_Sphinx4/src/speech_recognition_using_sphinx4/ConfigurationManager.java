0572622b7d35cad4e3ee62be94eb9b5bc995b6f3
neesha95 <neeshadananjalee@gmail.com>
2017-04-30 10:09:05 +0530

    Add files via upload

---
+++ Speech_Recognition_using_Sphinx4/src/speech_recognition_using_sphinx4/ConfigurationManager.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package speech_recognition_using_sphinx4;
+
+/**
+ *
+ * @author -Nsh-
+ */
+public class ConfigurationManager extends java.lang.Object implements java.lang.Cloneable{
+    
+    //ConfigurationManager configuration_manager = new ConfigurationManager();
+}
